a90fa73a748e54abea009fef38aab52e8ac3943a
Harsh Jain <hjain2083@gmail.com>
2024-08-21 23:56:36 +0530

    Add some API

--- server/models/product.js
+++ server/models/product.js
@@ -3,21 +3,50 @@ const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
     productName: {
-        type: String, 
-        require: true
-    },
-    category: {
         type: String,
         require: true
     },
-    prdouctPrice: {
+    productSku: {
+        type: String,
+        require: true,
+        unique: true 
+    },
+    productImages: {
+        type: [String],
+        require: true
+    },
+    productTagline: {
+        type: String,
+        require: true
+    },
+    variant: {
+        type: Object,
+        require: true
+    },
+    quantity: {
         type: Number,
         require: true
     },
-    availability: {
-        type: Boolean,
+    price: {
+        type: Number,
+        require: true
+    },
+    discount: {
+        type: Number,
+        require: true
+    },
+    productSummary: {
+        type: String,
+        require: true
+    },
+    productDescription: {
+        type: Object,
+        require: true
+    },
+    productCode: {
+        type: String,
         require: true
     }
-});
+}, { timestamps: true });
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model("product", productSchema);
