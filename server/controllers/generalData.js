d694d605f674158b13065d87255c709d23536f40
Harsh Jain <hjain2083@gmail.com>
2024-09-09 13:16:11 +0530

    generalData API Updation

--- server/controllers/generalData.js
+++ server/controllers/generalData.js
@@ -1,6 +1,6 @@
 const generalDataModel = require("../models/generaldata");
 
 module.exports.getCategoryData = async (req, res) => {
-    let categoryData = await generalDataModel.find();
+    let categoryData = (await generalDataModel.find())[0].productCategory;
     res.send(categoryData);
 };
