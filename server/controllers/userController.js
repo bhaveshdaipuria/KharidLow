73733d71e19383a7c31cd977b92ea44ba5ca45a8
Bhavesh Daipuria <bdaipuria@gmail.com>
2024-10-01 12:32:35 +0530

    implemented google oauth for login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user");
+const { OAuth2Client } = require("google-auth-library");
 
 module.exports.userRegister = async (req, res) => {
 	try {
@@ -91,4 +92,43 @@ module.exports.userLogin = async (req, res) => {
 module.exports.userLogout = (req, res) => {
 	res.clearCookie("token");
 	res.status(204).send();
+};
+
+module.exports.googleLogin = async (req, res) => {
+	try {
+		const { token } = req.body;
+		const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+		const ticket = await client.verifyIdToken({
+			idToken: token,
+			audience: process.env.GOOGLE_CLIENT_ID,
+		});
+		const payload = ticket.getPayload();
+		const email = payload.email;
+
+		const userExists = await userModel.findOne({ email: email });
+		if (!userExists) {
+			return res.status(404).json({
+				success: false,
+				message: "Account with this email does not exist",
+			});
+		}
+		const jwtToken = await jwt.sign(
+			{ email: userExists.email, userid: userExists._id },
+			process.env.JWT_SECRET,
+		);
+
+		if (!jwtToken) {
+			return res.status(404).json({ success: false, message: "Bad Request" });
+		}
+		res.cookie("token", jwtToken, {
+			httpOnly: false,
+			secure: true,
+		});
+		return res.status(200).json({ success: true, email });
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: "Token Verification Failed",
+		});
+	}
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { userLogin, userLogout, userRegister } = require("../controllers/userController");
+const {
+	userLogin,
+	userLogout,
+	userRegister,
+	googleLogin,
+} = require("../controllers/userController");
 
 router.post("/register", userRegister);
 router.post("/login", userLogin);
 router.get("/logout", userLogout);
+router.post("/google/login", googleLogin);
 
 module.exports = router;
